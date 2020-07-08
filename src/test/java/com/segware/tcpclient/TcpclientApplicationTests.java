cbd3e2d5085bb6c83908bb9dd0fcaeae483f24aa
romulo <romulosorato@hotmail.com>
2020-07-08 16:05:25 -0300

    first commit

---
+++ src/test/java/com/segware/tcpclient/TcpclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segware.tcpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
